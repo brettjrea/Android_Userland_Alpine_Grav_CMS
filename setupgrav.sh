ce87bce581f8aceb722676ce4022e00c4634073c
brettjrea <6890798+brettjrea@users.noreply.github.com>
2020-03-03 17:07:53 -0500

    add comments

--- setupgrav.sh
+++ setupgrav.sh
@@ -7,13 +7,9 @@ apk add git php7 php7-curl php7-ctype php7-dom php7-gd php7-json php7-mbstring p
 git config --global http.sslverify "false"
 ### install php composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-### 
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-###
 php composer-setup.php
-###
 php -r "unlink('composer-setup.php');"
-###
 mv composer.phar /usr/local/bin/composer
 ### clone grav repo 
 git clone -b master https://github.com/getgrav/grav.git
@@ -21,15 +17,15 @@ git clone -b master https://github.com/getgrav/grav.git
 cd ~/grav
 ### install dependecies
 composer install --no-dev -o
-###
+### install grav
 bin/grav install
-###
+### install grav admin
 bin/gpm install admin -y
-###
+### install git-sync
 bin/gpm install git-sync
-###
+### upgrade
 bin/gpm selfupgrade -f
-###
+### update
 bin/gpm update -f
 ### start php servers 
 php -S localhost:8000 system/router.php
